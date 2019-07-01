36604ca454b43a0733256dbf185bb797d1d14fbe
Vicky Law <vicky.law1217@gmail.com>
2019-07-01 16:53:55 -0400

    first creation

---
+++ assets/javascript/app.js
@@ -0,0 +1,129 @@
+$(document).ready(() => {
+
+//Front page to explain the game  
+    myMessage =
+    'You have total 10 questions. <br>' +
+    'Total time is 100 sec. <br>' +
+    'Each question you have 10 sec to anwser. <br>' +
+    'Good luck!';
+    
+    document.getElementById('message').innerHTML = myMessage;
+            
+    
+    
+});
+
+// Onclick start button
+$('#start').click(() => { 
+// Question show
+    $('#question').show();
+// Messages and Start Button hide
+    $('#message').hide();
+    $('#start').hide();
+
+    let myQuestions = [
+    
+        'Question 1. How many provinces in Canada?',
+        'Question 2. Which is the largest province in Canada?',
+        'abc',
+    ];
+   
+    document.getElementById('question').innerHTML = myQuestions;
+
+    for (let i = 0; i < myQuestion.length; i++) {
+        let randomQuestion = myQuestion(Math.floor(Math.random() * 4));
+    }
+});
+            
+           /* function quizQuestions() {
+                let showQuestion = document.getElementById('question').innerHTML = myQuestions.show();
+            }
+            
+            // Click for Start Button
+            
+            
+                
+            })
+            
+            gameIndex = 0;
+            myScore = 0;
+            timeIndex = 10;
+        gameQuestions = [];
+
+/*$('#question').show();
+
+    for (let i = 0; i < showQuestion.length; i++) {
+        let randomQuestion = showQuestion(Math.floor(Math.random() * 4));
+
+     
+       
+    }
+});
+
+
+
+    /*let myAnswers = [
+            ['a: Seven', 'b: Eight', 'c: Nine', 'd: Ten'],
+            ['a: Nunavut', 'b: Northwest Territories', 'c: Ontario', 'd: Newfoundland and labrador'],
+
+        ],
+
+        correctAnswer = [3, 1];
+    gameAnswer = [];
+
+
+
+
+
+
+
+        /*mySetUp._initialize = function () {
+            mySetUp.windowLoad();
+            startButton.addEventListener('click', mySetUp.gamePlay);
+            /*for (let i = 0; i < myQuestions.length; i++) {
+                myQuestions = Math.random();
+            }*/
+
+            // Click for Answer Buttons
+          //  for (var i = 0; i < buttonArray.length; i++) {
+        //buttonArray[i].addEventListener('click', mySetUp.answerClicked, false);
+   // }
+
+    // Score and Timer      
+    /*mySetUp.gamePlayData = function () {
+            mySetUp.gameIndex();
+            mySetUp.gameData();
+            score[0].textContent = myScore;
+            timer[0].textContent = timeIndex;
+        }
+
+    //Anwser Buttons
+    mySetUp.interfaceForGamePlay = function () {
+            let findAnswer = myAnswer[quizQuestions[gameIndex]];
+            for (let i = 0; i < findAnswer.length; i++) {
+                let a = ans[i];
+                buttonArray[i].textContent = a;
+            }
+        }
+
+    mySetUp.randomQuestionAndAnswer = function () {
+            let randomQuestion = Math.floor(Math.random() * 14);
+            if (gameQuestions.indexOf(randomQuestion) == -1) {
+                gameQuestions.push(randomQuestion);
+                gameAnswer.push(correctAnswer[randomQuestion]);
+            }
+
+        }
+
+
+
+}
+            
+            }*/
+    
+    
+        
+   
+        
+    
+
