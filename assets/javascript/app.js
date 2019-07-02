abda8c9e4e1105272734275ae65b05fbeca136d6
Vicky Law <vicky.law1217@gmail.com>
2019-07-02 17:06:58 -0400

    show random questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,33 +26,63 @@ $(document).ready(() => {
 
         }, 1000);
 
+
         // Messages and Start Button hide
         $('#message').hide();
         $('#start').hide();
 
+
     });
 
     i = 0;
-    myQuestion = [['How many provinces in Canada?', 'a', 'b', 'c', 'd'],
-    ['Which is the largest province in Canada?', 'm', 'a', 'd', 'z'],
-    
-    
+    myQuestion = [
+        ['How many provinces in Canada?', 'Seven', 'Eight', 'Nine', 'Ten'],
+        ['Which is the largest province in Canada?', 'm', 'a', 'd', 'z'],
+        ['Who was the first prime ministr of Canada?', 'Louis St.Laurent', 'Pierre Trudeau', 'Sir John A.Macdonald', 'Sir John Abbott'],
+
     ];
-    /*let myQuestionsLength = myQuestions.length;*/
+    /*let questionTracker = [];
+    let questionAmount = 3;
+    for (i = 0; i < myQuestion.length; i++) {
+        do {
+            let randomQuestion = Math.floor(Math.random() * myQuestion.length);
+        } while (existingQuestion());
+        display.innerHTML += myQuestion[randomQuestion];
+        questionTracker.push(randamQuestion);
+        function existingQuestion() {
+            for (i = 0; i < questionTracker.length; i++) {
+                if (questionTracker[i] === randamQuestion) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }*/
+
     function showQuestion() {
-        document.getElementById('myLabel').innerHTML = myQuestion[0][0];
-        myForm.opt1.value = myQuestion[i][1];
-        myForm.opt2.value = myQuestion[i][2];
-        myForm.opt3.value = myQuestion[i][3];
-        myForm.opt4.value = myQuestion[i][4];
+
+        let randomQuestion = Math.floor(Math.random() * myQuestion.length);
+
+        document.getElementById('myLabel').innerHTML = myQuestion[randomQuestion][0];
+        myForm.opt1.value = myQuestion[randomQuestion][1];
+        myForm.opt2.value = myQuestion[randomQuestion][2];
+        myForm.opt3.value = myQuestion[randomQuestion][3];
+        myForm.opt4.value = myQuestion[randomQuestion][4];
+
+        //document.getElementById('myLabel').innerHTML = myQuestion[randomQuestion][randomAnswer];
+        //myForm.opt1.value = myQuestion[i][1];
+        //myForm.opt2.value = myQuestion[i][2];
+        //myForm.opt3.value = myQuestion[i][3];
+        //myForm.opt4.value = myQuestion[i][4];
     }
-    function changeQuestion() {
-        i += 1;
-        /*for (i; i < myQuestion.length; i++) {
-            let randomQuestion = Math.floor(Math.random() * (myQuestion.length));*/
-   
-           
-    }
+
+    $('#nxtbtn').click(() => {
+        randam.shuffle(myQuestion);
+        /*Math.floor(Math.random() * (myQuestion.length));*/
+        document.getElementById('nxtbtn').innerHTML = myQuestion[0][0];
+
+
+    });
 
 
 
